09c2a7a0b4a0263b4b6cc63cb6bd6c7d743432f5
adebiyiemmanuel11@gmail.com <adebiyiemmanuel11@gmail.com>
2022-06-21 12:29:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = "Adebiyi Emmanuel"
+let height = "150cm";
+let country = "Nigeria, West Africa"
+
+document.write("My name is " + name)
+document.write("<br>");
+document.write("My height " + height);
+document.write("<br>");
+document.write("I am a citizen of " + country);
