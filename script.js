92dc4156058f5d787a9389461721524e0e4a2311
adebiyiemmanuel11@gmail.com <adebiyiemmanuel11@gmail.com>
2022-06-23 01:18:51 +0100

    adding alert function

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let name = "Adebiyi Emmanuel"
 let height = "150cm";
 let country = "Nigeria, West Africa"
 
+alert(name)
+alert(height)
+alert(country)
+
 document.write("My name is " + name)
 document.write("<br>");
 document.write("My height " + height);
